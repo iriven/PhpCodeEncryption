0d129d917d1c7618330c2e662783df136cde7dd1
Alfred Tchondjo <iriven@yahoo.fr>
2015-01-27 12:10:47 +0100

    Update exemple.php

--- Examples/exemple.php
+++ Examples/exemple.php
@@ -5,11 +5,22 @@ $encryption = new IrivenPhpCodeEncryption();
 * from file to file
 *
 */
+==> SPECIFIC FILE NAME
 $encryption->loadCode('source.php');
 $encryption->compileDatas();
 $encryption->save('encrypted.php');
-// save with auto-filename
+
+//chained command method
+$encryption->loadCode('source.php')->compileDatas()->save('encrypted.php');
+
+==> SAVE WITH AUTO-FILENAME
+$encryption->loadCode('source.php');
+$encryption->compileDatas();
 $encryption->save();
+
+//chained command method
+$encryption->loadCode('source.php')->compileDatas()->save();
+
 /**
 * from file to memory
 *
